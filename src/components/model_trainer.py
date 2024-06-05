4acf60a291f422fb078d836617f53a523bed7a9d
Mohit Saini <mohitsaini.nitk@gmail.com>
2024-06-05 19:07:38 +0530

    logging changed with model name

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -91,7 +91,6 @@ class ModelTrainer:
             best_model_score = max(sorted(model_report.values()))
 
             ## To get best model name from dict
-
             best_model_name = list(model_report.keys())[
                 list(model_report.values()).index(best_model_score)
             ]
@@ -100,6 +99,7 @@ class ModelTrainer:
             if best_model_score<0.6:
                 raise CustomException("No best model found")
             logging.info(f"Best found model on both training and testing dataset")
+            logging.info(best_model_name)
 
             save_object(
                 file_path=self.model_trainer_config.trained_model_file_path,
